fdd47138d76857aa5fef154e3d1f867c9f0dbde8
Leo <479711970@qq.com>
2017-12-31 00:33:24 +0800

    vue cli初始化 flex基本布局

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Content></Content>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Content from './components/Content.vue'
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Content
+  }
+}
+</script>
+
+<style>
+html,body{
+  height: 100%;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <div class="logo" stlye="font-family:font-family: 'Shadows Into Light Two', cursive;">
+      <span>QuickResume</span>
+    </div>
+    <div class="user">
+      <button class="login"><a href="">登录</a></button>
+      <button class="sign-up"><a href="">注册</a></button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+   
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+.logo{
+  font-size:26px;
+  font-style: italic;
+  display: -webkit-box;
+  vertical-align: super;
+  span{ 
+width: 440px; 
+color: #01E2C5; 
+text-shadow: 0 1px 1px  green;
+
+  }
+}
+header{
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  box-shadow: 0 3px 6px  rgba(0,0,0,0.4);
+}
+.login,.sign-up {
+  color:white;
+  border-radius: 4px;
+  background: #1DA496;
+  border:none;
+  margin-left:10px;
+  a{
+    display: inline-block;
+    padding: 10px 20px;
+    cursor: pointer;
+    
+  }
+   &:hover{
+     background:rgb(97, 202, 167);
+   }
+}
+
+</style>
